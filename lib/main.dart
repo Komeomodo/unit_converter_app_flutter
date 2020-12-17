99c395cfe69fb25bbbdae53395df4cfad4cb52bd
kome <omodo.oghenekome@decagon.dev>
2020-12-17 19:14:41 +0100

    update commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:unit_converter_app_flutter/category.dart';
 
+const _categoryName = 'Cake';
+const _categoryIcon = Icons.cake;
+const _categoryColor = Colors.green;
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Rectangle'),
-        ),
-        body: HelloRectangle(),
-      ),
-    ),
-  );
+  runApp(UnitConverterApp());
 }
 
-class HelloRectangle extends StatelessWidget {
+class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.teal[200],
-        height: 400.0,
-        width: 300.0,
-        child: Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(fontSize: 40.0),
-            textAlign: TextAlign.center,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Unit Converter',
+            style: TextStyle(
+              fontSize: 30.0,
+            ),
+          ),
+          backgroundColor: Colors.green[100],
+        ),
+        body: Center(
+          child: Category(
+            name: _categoryName,
+            color: _categoryColor,
+            iconLocation: _categoryIcon,
           ),
         ),
       ),
